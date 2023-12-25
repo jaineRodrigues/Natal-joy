869366138e68b3a0b1d9441ac85f800523ca7464
jaineRodrigues <jainer469@gmail.com>
2023-12-24 21:02:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("present-link").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    alert("Desejo a você um maravilhoso Natal e um próspero Ano Novo! Que o seu próximo ano seja repleto de alegria, felicidade e momentos especiais! 🎄🎁 🎉🎄");
+});
